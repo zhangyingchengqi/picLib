1f97a18df98431a9b9bd5973c28aa3722d44f908
zy <1069595532@qq.com>
2020-11-19 18:58:21 +0800

    完成了bus及配置中心高可用

--- base-microservice-config3/src/main/java/com/yc/config/BaseMicroserviceConfig3.java
+++ base-microservice-config3/src/main/java/com/yc/config/BaseMicroserviceConfig3.java
@@ -3,15 +3,13 @@ package com.yc.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
-public class ConfigBusApp {
+public class BaseMicroserviceConfig3 {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigBusApp.class, args);
+        SpringApplication.run(BaseMicroserviceConfig3.class, args);
     }
 }
 
--- base-microservice-eureka/src/main/java/com/yc/picLib/EurekaApp.java
+++ base-microservice-eureka/src/main/java/com/yc/picLib/EurekaApp.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer   //启用 eureka服务器
-public class ConfigEurekaClientApp {
+public class EurekaApp {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigEurekaClientApp.class, args);
+        SpringApplication.run(EurekaApp.class, args);
     }
 }
