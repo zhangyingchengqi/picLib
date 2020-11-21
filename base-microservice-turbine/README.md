5e3f4acf5c3f2cdce001bb10b586319f6a4f5704
zy <1069595532@qq.com>
2020-11-21 14:10:15 +0800

    微服务

---
+++ base-microservice-turbine/README.md
@@ -0,0 +1,3 @@
+启动 turbine后，将以下地址输入到 dashboard -> http://localhost:9001/ 中即可
+
+http://localhost:9101/turbine.stream
---
+++ piclib-domain/src/main/java/com/yc/piclib/entity/Pic.java
@@ -0,0 +1,16 @@
+package com.yc.piclib.entity;
+
+import lombok.Data;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Data      //  lombok 注解，节省getter, setter
+@Table(name = "pic")
+public class Pic {
+    @Id  // JPA注解,指定此属性为表中的主键
+    private Integer id;   //注意: 对应的数据表中的字段名叫 book_id
+    private String path;
+
+}
